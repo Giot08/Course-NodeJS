3228c087fd1ea167f57934374b96b77cf47f9042
Giot08 <xgiot08@gmail.com>
2022-07-29 15:10:33 -0400

    70

--- 05-climaApp/index.js
+++ 05-climaApp/index.js
@@ -1,12 +1,34 @@
 const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 const main = async () => {
+  
+  const busquedas = new Busquedas();
   let opt;
+
 
   do {
     opt = await inquirerMenu();
 
-    console.log({ opt });
+    switch(opt){
+      case 1: 
+      //mostrar mensaje
+      const lugar = await leerInput('Ciudad: ');
+      console.log(lugar);
+      // buscar el lugar
+      // seleccionar el lugar
+      // obtener datos del clima
+      // mostrar resultados
+
+      console.log('\n informacion: \n' .cyan)
+      console.log('Ciudad:', )
+      console.log('Lat:', )
+      console.log('Long:', )
+      console.log('Temp:', )
+      console.log('T-Max:', )
+      console.log('T-Min:', )
+      break
+    }
 
     if (opt !== 0) await pausa();
   } while (opt !== 0);
---
+++ 05-climaApp/models/busquedas.js
@@ -0,0 +1,17 @@
+class Busquedas {
+    historial = ['Barquisimeto', 'Iquique'];
+
+    constructor(){
+        //Leer db
+
+    }
+    async ciudad (lugar = ''){
+        // peticion HTTP
+        console.log(lugar)
+
+        return []; //Retorna las ciudades que coinciden
+    }
+}
+
+
+module.exports = Busquedas;
