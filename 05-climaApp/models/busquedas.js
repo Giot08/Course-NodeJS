5dfeb1f87b06ced2e965d58363853cb05f91328e
Giot08 <xgiot08@gmail.com>
2022-07-29 16:38:34 -0400

    76

--- 05-climaApp/models/busquedas.js
+++ 05-climaApp/models/busquedas.js
@@ -1,35 +1,34 @@
-const axios = require('axios');
+const axios = require("axios");
 
 class Busquedas {
-    historial = ['Barquisimeto', 'Iquique'];
+  historial = ["Barquisimeto", "Iquique"];
 
-    constructor(){
-        //Leer db
+  constructor() {
+    //Leer db
+  }
+  async ciudad(lugar = "") {
+    const api = process.env.API_KEY;
 
-    }
-    async ciudad (lugar = ''){
-        
-        try {
-            // peticion HTTP
-/*
+    try {
+      // peticion HTTP
+      /*
             //No funciona porque no tiene parametros
             const instance = axios.create({
               baseURL: `https://restcountries.com/v3.1/${lugar}/`  
             });
 
             const resp = await instance.get() */
-            const resp = await axios.get(`https://restcountries.com/v3.1/name/${lugar}`)
-            console.log(resp)
-            console.log('hola!')
-            
-            return []; //Retorna las ciudades que coinciden
-        } catch (error) {
-            return
-            
-        }
+      //En su lugar uso la api de restcountries
+      const resp = await axios.get(`${api}${lugar}`);
+      //   const resp = await axios.get(`${api}${lugar}`);
+      console.log(api);
+      console.log(resp.data);
 
+      return []; //Retorna las ciudades que coinciden
+    } catch (error) {
+      return;
     }
+  }
 }
-
 
 module.exports = Busquedas;
