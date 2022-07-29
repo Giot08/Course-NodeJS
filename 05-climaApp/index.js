f418cde9bf21e9d7682d8f44a2cf513fb94d8109
Giot08 <xgiot08@gmail.com>
2022-07-29 14:57:28 -0400

    67

--- 05-climaApp/index.js
+++ 05-climaApp/index.js
@@ -1,18 +1,9 @@
-const {
-  inquirerMenu,
-  pausa,
-} = require("./helpers/inquirer");
+const { leerInput } = require("./helpers/inquirer")
+
+
 
 const main = async () => {
-  let opt;
-
-  do {
-
-    opt = await inquirerMenu();
-    console.log({opt});
-
-    if (opt !== 0) await pausa();
-  } while (opt !== 0);
-};
+  const texto = await leerInput('hola: ');
+}
 
 main();
