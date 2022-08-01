58aea397cfa7f59a24b9c423076a71a6ea32ab40
Jose Miguel Torres <96963293+Giot08@users.noreply.github.com>
2022-08-01 19:37:46 -0400

    88

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,10 +1,34 @@
-const http = require('http');
+const http = require("http");
+const { stringify } = require("querystring");
 
-const server = http.createServer((req, res) =>{
+//const server =
+http
+  .createServer((req, res) => {
+
+    const error = {
+        tipo: "error",
+        nro: 404,
+        mensaje: "Pagina no encontrada"
+    }
     
-    res.write('Hello World');
-    res.end()
-})
-.listen('8080');
 
-console.log('listening server 8080')
+    
+    // res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    res.setHeader('Content-Disposition', 'attachment; filename=archivo.csv');
+    res.writeHead(200, {'Content-Type': 'application/csv'}); //para descargar archivos
+    /*
+    res.writeHead(200, {'Content-Type': 'application/json'});
+
+    res.write(JSON.stringify(error)); */
+    res.write('id, Nombre\n')
+    res.write('1, maria\n')
+    res.write('2, jose\n')
+    res.write('3, fran\n')
+
+
+    res.end();
+  })
+  .listen(8080);
+
+console.log("listening server 8080");
