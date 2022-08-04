b7fa7816e98b5c6e545b038e88dd57958397ef38
Jose Miguel Torres <96963293+Giot08@users.noreply.github.com>
2022-08-03 21:48:14 -0400

    102

--- 08-rest-server/app.js
+++ 08-rest-server/app.js
@@ -1,12 +1,7 @@
-
 require("dotenv").config();
 
+const Server = require("./models/server");
 
+const server = new Server();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+server.listen();
--- 08-rest-server/models/server.js
+++ 08-rest-server/models/server.js
@@ -1,11 +1,33 @@
 const express = require("express");
 
-
 class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
 
-    constructor(){
-        this.app = express();
-    }
+    // Middleware
+    this.middleware();
+
+    // Rutas de mi app
+    this.routes();
+  }
+  //   funciones de la clase
+
+  middleware() {
+    // Directorio pub
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.get("/api", (req, res) => {
+      res.send("Hello World");
+    });
+  }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Server is running on port ${this.port}`);
+    });
+  }
 }
 
 module.exports = Server;
