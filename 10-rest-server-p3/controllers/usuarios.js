1f54f474ccaa2b918290d944e6a448d154cb0cd1
Jose Miguel Torres <96963293+Giot08@users.noreply.github.com>
2022-08-05 02:20:23 -0400

    135

---
+++ 10-rest-server-p3/controllers/usuarios.js
@@ -0,0 +1,93 @@
+const { response, request } = require("express");
+const bcryptjs = require("bcryptjs");
+
+const Usuario = require("../models/usuario");
+
+const usuariosGet = async (req = request, res = response) => {
+  // const { q, nombre = "No name", apikey, page = 1, limit = 5 } = req.query;
+
+  const { limite = 5, desde = 0 } = req.query;
+  const queryEstado = { estado: true };
+
+  /*
+  const usuarios = await Usuario.find(queryEstado)
+  .skip(Number(desde))
+  .limit(Number(limite))
+  ;
+
+  const total = await Usuario.countDocuments(queryEstado); 
+  
+  res.json({
+    total,
+    usuarios
+  });
+  */
+
+  const [total, usuarios] = await Promise.all([
+    Usuario.countDocuments(queryEstado), // total
+    Usuario.find(queryEstado).skip(Number(desde)).limit(Number(limite)), //usuarios
+  ]);
+
+  res.json({
+    total,
+    usuarios,
+  });
+};
+
+const usuariosPut = async (req, res) => {
+  const { id } = req.params;
+  const { _id, password, google, correo, ...resto } = req.body;
+
+  // Validad contra db
+  if (password) {
+    // encriptar contraseña
+    const salt = bcryptjs.genSaltSync();
+    resto.password = bcryptjs.hashSync(password, salt);
+  }
+
+  const usuario = await Usuario.findByIdAndUpdate(id, resto);
+
+  res.json(usuario);
+};
+
+const usuariosPost = async (req, res) => {
+  const { nombre, correo, password, rol } = req.body;
+  const usuario = new Usuario({ nombre, correo, password, rol });
+
+  // encriptar contraseña
+  const salt = bcryptjs.genSaltSync();
+  usuario.password = bcryptjs.hashSync(password, salt);
+
+  // Guardar en base de datos
+  await usuario.save();
+
+  res.json({
+    msg: "post api - controlador",
+    usuario,
+  });
+};
+
+const usuariosDelete = async (req, res) => {
+  const { id } = req.params;
+
+  // Lo borramos totralmente
+  // const usuario = await Usuario.findByIdAndDelete(id); // Lo borra de la db
+
+  const usuario = await Usuario.findByIdAndUpdate(id, { estado: false });
+
+  res.json(usuario);
+};
+
+const usuariosPatch = (req, res) => {
+  res.json({
+    msg: "patch api - controlador",
+  });
+};
+
+module.exports = {
+  usuariosGet,
+  usuariosPut,
+  usuariosPost,
+  usuariosDelete,
+  usuariosPatch,
+};
---
+++ 10-rest-server-p3/models/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+
+const { dbConnection } = require("../database/config");
+
+class Server {
+  constructor() {
+    
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usuariosPath = "/api/usuarios";
+
+    // Conectar a db
+    this.conectarDB();
+
+    // Middleware
+    this.middleware();
+
+    // Rutas de mi app
+    this.routes();
+  }
+  //   funciones de la clase
+
+  async conectarDB() {
+    await dbConnection();
+  }
+
+  middleware() {
+    // Cors
+    this.app.use(cors());
+
+    // Parseo y lectura
+    this.app.use(express.json());
+
+    // Directorio pub
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.use(this.usuariosPath, require("../routes/usuarios"));
+  }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Server is running on port ${this.port}`);
+    });
+  }
+}
+
+module.exports = Server;
