d5bd38a92cf1a03553cba016cc694e8f89b4618d
Jose Miguel Torres <96963293+Giot08@users.noreply.github.com>
2022-08-05 22:13:34 -0400

    142

--- 10-rest-server-p3/controllers/auth.js
+++ 10-rest-server-p3/controllers/auth.js
@@ -1,9 +1,48 @@
 const { response } = require("express");
+const bcryptjs = require("bcryptjs");
 
-const login = (req, res = response) => {
-  res.json({
-    msg: "Login ok",
-  });
+const Usuario = require("../models/usuario");
+
+const login = async (req, res = response) => {
+  const { correo, password } = req.body;
+
+  try {
+    // Verificar si el email existe
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      return res.status(404).json({
+        msg: "Usuario / Password no son correctos",
+      });
+    }   
+    
+    
+    // Ver si el usuario esta activo actualmente
+    if (!usuario.estado) {
+      return res.status(404).json({
+        msg: "El usuario no esta registrado, Estado: false",
+      });
+    }
+
+    // Verificar si el password es correcto
+    const validPassword = bcryptjs.compareSync(password, usuario.password);
+    if (!validPassword) {
+      return res.status(404).json({
+        msg: "Usuario / Password no son correctos - Password",
+      });
+    }
+
+
+    // Generar el JWT
+
+    res.json({
+      msg: "Login ok",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Hubo un error, contacte el admin",
+    });
+  }
 };
 
 module.exports = {
